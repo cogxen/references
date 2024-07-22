37bf129abf285b3379fb473e1a629d22ebb32d5c
noeyislearning <hello@noeyislearning.dev>
2024-07-22 10:51:50 +0800

    style: update submission item anchor element styling

--- src/scripts/core.js
+++ src/scripts/core.js
@@ -6,7 +6,7 @@ async function loadSubmissions() {
 
   data.submissions.forEach((submission) => {
     const section = document.createElement("section")
-    section.className = "w-full flex flex-col items-start gap-2 py-4"
+    section.className = "w-full max-w-3xl mx-auto flex flex-col items-center gap-2 py-4"
 
     const span = document.createElement("span")
     span.className = "text-lg w-full text-center"
@@ -14,16 +14,18 @@ async function loadSubmissions() {
     section.appendChild(span)
 
     const divContainer = document.createElement("div")
-    divContainer.className = "w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2"
+    divContainer.className = "w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2"
 
     submission.items.forEach((item) => {
       const anchor = document.createElement("a")
       anchor.href = item.url
       anchor.target = "_blank"
-      anchor.className = `max-w-xl w-full border-4 px-4 py-2 text-base font-bold text-slate-600 ${
+      anchor.className = `max-w-xl w-full px-4 py-2 text-base font-bold text-slate-600 leading-4 ${
         item.isLecture
-          ? "bg-slate-100 border-slate-600 hover:bg-slate-300"
-          : "bg-emerald-100 border-emerald-600 hover:bg-emerald-300"
+          ? "bg-slate-100 hover:bg-slate-300"
+          : item.isProject
+          ? "bg-amber-100 hover:bg-amber-300"
+          : "bg-emerald-100 hover:bg-emerald-300"
       } flex flex-col justify-between`
 
       const itemDiv = document.createElement("div")
